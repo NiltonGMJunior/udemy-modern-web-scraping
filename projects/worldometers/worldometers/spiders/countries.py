e673f342b3dabec22dad090b65bfe975cd5ef932
Nilton Junior <ngm.junior@outlook.com>
2022-01-31 20:57:44 -0300

    Debugging countries spider

--- projects/worldometers/worldometers/spiders/countries.py
+++ projects/worldometers/worldometers/spiders/countries.py
@@ -4,7 +4,8 @@ import logging
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info']
-    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+    start_urls = [
+        'https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
         countries = response.xpath("//td/a")
@@ -13,10 +14,12 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield response.follow(url=link, callback=self.parse_country, cb_kwargs={"country_name": name})
+            yield response.follow(url=link, callback=self.parse_country, meta={"country_name": name})
 
-    def parse_country(self, response, country_name):
-        rows = response.xpath("//table[@class='table table-striped table-bordered table-hover table-condensed table-list'][1]/tbody/tr")
+    def parse_country(self, response):
+        country_name = response.request.meta["country_name"]
+        rows = response.xpath(
+            "//table[@class='table table-striped table-bordered table-hover table-condensed table-list'][1]/tbody/tr")
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
             population = row.xpath(".//td[2]/strong/text()").get()
