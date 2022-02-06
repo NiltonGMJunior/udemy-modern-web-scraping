cfc28020c5e8988e77100367fb1858a0fc8d7368
Nilton Junior <ngm.junior@outlook.com>
2022-02-06 18:43:04 -0300

    Section 13

--- projects/imdb/imdb/pipelines.py
+++ projects/imdb/imdb/pipelines.py
@@ -5,9 +5,65 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import pymongo
+import sqlite3
 
 
-class ImdbPipeline:
+class MongoDBPipeline:
+    collection_name = "best_movies"
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(
+            "mongodb+srv://admin:admin@imdb.fluvw.mongodb.net/imdb?retryWrites=true&w=majority")
+        self.db = self.client["imdb"]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(item)
+        return item
+
+
+class SQLitePipeline:
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect('imdb.db')
+        self.c = self.connection.cursor()
+        try:
+            self.c.execute('''
+                CREATE TABLE 
+                        best_movies(
+                        title TEXT,
+                        year TEXT,
+                        duration TEXT,
+                        genre TEXT,
+                        rating TEXT,
+                        movie_url TEXT
+                    )
+            ''')
+        except:
+            pass
+        self.connection.commit()
+
+    def close_spider(self, spider):
+        self.connection.close()
+
+    def process_item(self, item, spider):
+        self.c.execute(
+            '''
+                INSERT INTO 
+                    best_movies(title, year, duration, genre, rating, movie_url)
+                VALUES
+                    (?, ?, ?, ?, ?, ?)
+            ''', 
+            (
+                item.get("title"),
+                item.get("year"),
+                item.get("duration"),
+                item.get("genre"),
+                item.get("rating"),
+                item.get("movie_url"),
+            )
+        )
+        self.connection.commit()
         return item
